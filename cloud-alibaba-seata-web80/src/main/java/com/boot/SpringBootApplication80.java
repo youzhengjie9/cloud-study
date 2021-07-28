5f6b6528e9c43b6782c580259f11d15bf96b59d7
ms666 <1550324080@qq.com>
2021-07-29 01:33:01 +0800

    2021/7/29 springCloud alibaba seata分布式事务

---
+++ cloud-alibaba-seata-web80/src/main/java/com/boot/SpringBootApplication80.java
@@ -0,0 +1,17 @@
+package com.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
+@EnableFeignClients
+public class SpringBootApplication80 {
+
+  public static void main(String[] args) {
+      SpringApplication.run(SpringBootApplication80.class,args);
+  }
+}
